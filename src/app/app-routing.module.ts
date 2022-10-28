58651a137c1f3c70f1260203abb79596e337bf82
Jawad <mdjawad4465@gmail.com>
2022-10-28 16:39:02 +0530

    changes in routing.module and app.component.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:CardsComponent
+
+  },
+  {
+    path:'cards',
+    component:CardsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
